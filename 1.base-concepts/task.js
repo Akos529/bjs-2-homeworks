a50075d85039da3bc8d1de46e04da62efb7960da
Taisia Ivanovskaya <taya.naumova@gmail.com>
2023-01-15 21:10:47 +0300

    Задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - (4*a*c);
+  if (d >= 0){
+    if (d > 0){
+      arr[0] = (-b + Math.sqrt(d) )/(2*a);
+      arr[1] = (-b - Math.sqrt(d) )/(2*a);      
+    }
+  } arr [0] = -b/(2*a);
+
   return arr;
 }
 
