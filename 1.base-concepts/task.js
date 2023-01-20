bc72409e53959f7c70c3929183b77ea727203455
Taisia Ivanovskaya <taya.naumova@gmail.com>
2023-01-20 22:52:14 +0300

    Задание 1 исправлено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,13 +2,15 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = Math.pow(b, 2) - (4*a*c);
-  if (d >= 0){
-    if (d > 0){
-      arr[0] = (-b + Math.sqrt(d) )/(2*a);
-      arr[1] = (-b - Math.sqrt(d) )/(2*a);      
+  if (d > 0){
+    arr[0] = (-b + Math.sqrt(d) )/(2*a);
+    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+    return arr;   
+  if (d = 0){
+    arr [0] = -b/(2*a);
+    return arr;
     }
-  } arr [0] = -b/(2*a);
-
+  } 
   return arr;
 }
 
