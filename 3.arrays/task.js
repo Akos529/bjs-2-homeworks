df7a67fe6ea7a8bfc708bab12f891903b28a6bc9
Taisia Ivanovskaya <taya.naumova@gmail.com>
2023-02-01 23:17:45 +0300

    Задание Массивы выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    const compareArrays = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+    return compareArrays;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let filteredUsers = users.filter(user => user.gender === gender);
+    let ages = filteredUsers.map(filteredUser => filteredUser.age);
+    let result = ages.reduce((prev, age) => prev + age, 0) / ages.length;
+    if (isNaN(result)){
+        return 0;
+    }
+    return result;  
 }
