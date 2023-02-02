394dcd69f6422df06ba27dd90e0bb95e0f4545fa
Taisia Ivanovskaya <taya.naumova@gmail.com>
2023-02-03 00:33:58 +0300

    Задание Объекты выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks !== undefined){
+        this.marks.push(...marks)
+    } 
+    return;
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks !== undefined && this.marks !== 0){
+        let average = this.marks.reduce((prev, mark) => prev + mark, 0) / this.marks.length;
+        if (isNaN(average)){
+        return 0;
+        }
+        return average; 
+    }
+    return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.marks;
+    delete Student.prototype.subject;
+    this.excluded = reason;  
 }
